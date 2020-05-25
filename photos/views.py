16e027894662ed0bdf5269356ec51aa0ec91fba9
moselamar <44118179+moselamar@users.noreply.github.com>
2020-05-25 11:35:56 +0300

    seacrch function

--- photos/views.py
+++ photos/views.py
@@ -13,17 +13,17 @@ def photo_display(request):
 
 
 def search_results(request):
-
-    if 'image' in request.GET and request.GET["image"]:
-        search_term = request.GET.get("image")
-        searched_images = Image.search_by_category(search_term)
+    
+    if 'searchItem' in request.GET and request.GET["searchItem"]:
+        search_term = request.GET.get("searchItem")
+        searched_image = Image.search_by_category(search_term)
         message = f"{search_term}"
 
-        return render(request,'search.html',{"message":message,"images":searched_images})
+        return render(request, 'index.html',{"message":message,"all_images": searched_image})
 
     else:
-        message="You haven't searched for any term"
-        return render(request,'search.html',{"message":message})
+        message = "You haven't searched for any term"
+        return render(request, 'index.html',{"message":message})
 
 
 def get_category(request,category):
